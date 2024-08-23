2747e6a5ce55d0dca5d0e1d0e3d9bc48aa470470
ironshine <cjfgml2320@gmail.com>
2024-08-23 15:25:54 +0900

    init commit

---
+++ src/main/java/com/sparta/jpaupgradeschedule/JpaUpgradeScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpaupgradeschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaUpgradeScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaUpgradeScheduleApplication.class, args);
+    }
+
+}
